ffffa55ffbc8c9eae0f4d36d995582c6b6e06f99
Gavrilajava <georgy.gavrilchik@gmail.com>
2020-04-10 08:50:25 -0500

    delete state from counters

--- src/components/Counters.js
+++ src/components/Counters.js
@@ -2,34 +2,6 @@ import React from 'react'
 
 export default class Counters extends React.Component {
 
-  constructor(){
-    super()
-    this.state = {
-      counters: []
-    }
-  }
-
-  componentDidMount(){
-    if (this.props.counters){
-      this.setState({
-        counters: this.props.counters,
-        title: this.props.title,
-        description: this.props.description
-      })
-    }
-    else{
-      this.setState({
-        counters: [
-          {name: "Job Postings", value: 30},
-          {name: "Notes", value: 299},
-          {name: "Companies", value: 25},
-          {name: "Tasks", value: 35}
-        ],
-        title: "Your Job Search Stats",
-        description: "This is default values for counters. You should provide something as props or change code at /components/Counters.js"
-      })
-    }
-  }
 
   renderCounter(counter){
     return(
@@ -49,18 +21,29 @@ export default class Counters extends React.Component {
           <div className="row mb-5 justify-content-center">
             <div className="col-md-7 text-center">
               <h2 className="section-title mb-2 text-white">
-                {this.state.title}
+                {this.props.title}
               </h2>
               <p className="lead text-white">
-                {this.state.description}
+                {this.props.description}
               </p>
             </div>
           </div>
           <div className="row pb-0 block__19738 section-counter">
-            {this.state.counters.map(counter => this.renderCounter(counter))}
+            {this.props.counters.map(counter => this.renderCounter(counter))}
           </div>
         </div>
       </section>
     )
   }
+}
+
+Counters.defaultProps = {
+  counters: [
+    {name: "Job Postings", value: 30},
+    {name: "Notes", value: 299},
+    {name: "Companies", value: 25},
+    {name: "Tasks", value: 35}
+  ],
+  title: "Your Job Search Stats",
+  description: "This is default values for counters. You should provide something as props or change code at /components/Counters.js"
 }
